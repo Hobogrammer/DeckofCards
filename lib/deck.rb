3a8de0825a28a7f503a2ad97397abd1017e51599
Andre Smith <lordsilent@gmail.com>
2014-02-01 01:30:37 -0800

    Cards, shuffling, and a test

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,14 +6,29 @@ class Deck
   end
 
   def set_cards
-
+    suits = [ 'spades', 'hearts', 'diamonds', 'clubs']
+    counter = 1
+    suits.each do |suit|
+      (2..10).each do |number|
+        @cards["#{counter}"] = {  "suit" => "#{suit}", "number" => number }
+        counter += 1
+      end
+    end
   end
 
   def shuffle_cards
-
+    @cards = @cards.to_a.shuffle.to_h
   end
 
-  def deal_cards
-
+  def show_cards
+    @cards.each do |k,v|
+      puts "#{v['number']} of #{v['suit']}"
+    end
   end
 end
+
+playing_cards = Deck.new
+playing_cards.set_cards
+playing_cards.show_cards
+playing_cards.shuffle_cards
+playing_cards.show_cards
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 require 'deck'
 
-describe "A deck of cards with no jokers,kings,and queens" do
-  it has "36 cards" do
+describe "A deck of cards with no jokers,kings,and queens, or aces" do
+  it "has 36 cards" do
     deck = Deck.new
+    deck.set_cards
 
     deck.cards.length.must_equal 36
   end
