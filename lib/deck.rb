8a226e2acf5647c11a6594a133cb24ff161d7034
Andre Smith <andrepsmithjr@gmail.com>
2014-01-31 21:47:22 -0800

    Inital commit

---
+++ lib/deck.rb
@@ -0,0 +1,19 @@
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = {}
+  end
+
+  def set_cards
+
+  end
+
+  def shuffle_cards
+
+  end
+
+  def deal_cards
+
+  end
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+require 'deck'
+
+describe "A deck of cards with no jokers,kings,and queens" do
+  it has "36 cards" do
+    deck = Deck.new
+
+    deck.cards.length.must_equal 36
+  end
+end
